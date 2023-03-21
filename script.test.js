fc98ada1cf6a9630285fd21f6e144d1edb08d752
FarahHachicha <farah.hachicha@ensi-uma.tn>
2023-03-21 09:21:10 +0100

    add test

---
+++ script.test.js
@@ -0,0 +1,30 @@
+import { assert } from 'https://deno.land/std/testing/asserts.ts';
+
+// Importer le code source
+import { display, buttons } from './script.js';
+
+// Définir les tests
+Deno.test('Test clear button', () => {
+  display.innerText = '123';
+  buttons.find(button => button.innerText === 'C').click();
+  assert(display.innerText === '');
+});
+
+Deno.test('Test evaluate button', () => {
+  display.innerText = '2 + 2';
+  buttons.find(button => button.innerText === '=').click();
+  assert(display.innerText === '4');
+});
+
+Deno.test('Test delete button', () => {
+  display.innerText = '123';
+  buttons.find(button => button.innerText === '←').click();
+  assert(display.innerText === '12');
+});
+
+Deno.test('Test adding digit button', () => {
+  display.innerText = '12';
+  buttons.find(button => button.innerText === '3').click();
+  assert(display.innerText === '123');
+});
+
